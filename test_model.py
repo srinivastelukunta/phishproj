152b27e8603087d9619f7311b92f0ffe66b42a07
Srini T <32458028+srinivastelukunta@users.noreply.github.com>
2024-08-07 00:47:41 +0530

    Srini Mod 5

--- test_model.py
+++ test_model.py
@@ -92,3 +92,4 @@ def test_model_persistence(trained_model):
     scaled_result = loaded_scaler.transform(user_age_salary)
     res = loaded_classifier.predict(scaled_result)
     assert res in [0, 1]
+    
