2bebec4bd29bedc0f61ecc2b950ce41e66c9acfc
Srini T <32458028+srinivastelukunta@users.noreply.github.com>
2024-08-07 01:11:41 +0530

    Srini Mod 7

--- app.py
+++ app.py
@@ -3,39 +3,40 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import json
 from sklearn.preprocessing import StandardScaler
+import os
+
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
-scaler=pickle.load(open('scaler.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     user_age_salary = [int(x) for x in request.form.values()]
-    user_age_salary1=[user_age_salary]
+    user_age_salary1 = [user_age_salary]
     scaled_result = scaler.transform(user_age_salary1)
     prediction = model.predict(scaled_result)
-    if prediction==1:
+    if prediction == 1:
         return render_template('index.html', prediction_text='Yes, Purchaser')
     else:
         return render_template('index.html', prediction_text='No, Not a Purchaser')
 
-
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.get_json()
-    age=data['age']
-    salary=data['salary']
-    user_age_salary=[[age,salary]]
+    age = data['age']
+    salary = data['salary']
+    user_age_salary = [[age, salary]]
     scaled_result = scaler.transform(user_age_salary)
-    res=model.predict(scaled_result)
-    if res==1:
-        return jsonify({'Sales Prediction': 'Yes, Purchaser' })
+    res = model.predict(scaled_result)
+    if res == 1:
+        return jsonify({'Sales Prediction': 'Yes, Purchaser'})
     else:
-        return jsonify({'Sales Prediction': 'No, Not a Purchaser' })
-    
+        return jsonify({'Sales Prediction': 'No, Not a Purchaser'})
 
 if __name__ == "__main__":
-    app.run(debug=True,use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
