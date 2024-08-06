36283974348681960b6843d9c77b1625b79d96c7
Srini T <32458028+srinivastelukunta@users.noreply.github.com>
2024-08-07 00:41:22 +0530

    Srini Mod 3

--- test_model.py
+++ test_model.py
@@ -1,7 +1,4 @@
-# tests/test_model.py
-
 import pytest
-import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
